18f1b01e910f8973007ab649872b44920147e39a
Jia <469944061@qq.com>
2022-10-16 21:12:55 +0800

    first commit

---
+++ src/com/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.git;
+
+/**
+ * @author Faith 信仰
+ * @create 2022-10-16 21:06
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
